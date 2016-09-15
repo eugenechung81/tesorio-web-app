082634c735fbeff3b3ca3ba9f7cff76d118504a9
Eugene <eugenech@gmail.com>
2016-09-15 11:32:25 -0400

    Update

--- server/server.py
+++ server/server.py
@@ -1,11 +1,19 @@
 import sys
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/api/user')
+def get_user():
+    user = {
+        'user_id': 'eugene',
+        'name': 'Eugene'
+    }
+    return jsonify(user)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'dev':
